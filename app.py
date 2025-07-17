42010cce8a0bd9e641b111abf76ac260e0a24914
Balaji42003 <gandalabalaji@gmail.com>
2025-07-17 14:50:42 +0530

    commit

--- app.py
+++ app.py
@@ -220,5 +220,7 @@ def process_audio():
         if 'temp_audio_path' in locals() and temp_audio_path and os.path.exists(temp_audio_path):
             os.remove(temp_audio_path)
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Render sets PORT env var
+    app.run(host="0.0.0.0", port=port)
